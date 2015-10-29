bee1f57e9ede65d1fe6f9cad1838fb57feda19ff
hori <horitomoki@mokoti-no-MacBook-Pro.local>
2015-10-29 14:28:26 +0900

    1st commit

---
+++ SARUTOBI/Assets/_myScript/UnityBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityBehaviour : MonoBehaviour {
+    
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("up")) {
+			transform.position += transform.forward * 0.1f;
+			animator.SetBool("Run", true);
+		} else {
+			animator.SetBool("Run", false);
+		}
+		if (Input.GetKeyDown("right")) {
+			transform.Rotate(0,  180, 0);
+		}
+		if (Input.GetKeyDown ("left")) {
+			transform.Rotate(0, 180, 0);
+		}
+	}
+}
